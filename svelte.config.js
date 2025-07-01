46d2c86dcfed9c5fc626edd87b251889e72f9690
Vhj9313 <jadhavaibhav9313@gmail.com>
2025-07-02 01:12:19 +0530

    Minor changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
-
-
-let adapter;
-if (process.env.VERCEL) {
-  adapter = import('@sveltejs/adapter-static').then((module) => module.default);
-} else {
-  adapter = import('@sveltejs/adapter-static').then((module) => module.default);
-}
 
 export default {
   preprocess: [vitePreprocess(), sveltePreprocess()],
 
   kit: {
-    // Use dynamic import for the adapter
-    adapter: adapter,
+    adapter: adapter(),
 
     paths: {
-      base: '', // Set the base path if required
+      base: '', // adjust if deployed in subfolder
     },
 
     prerender: {
