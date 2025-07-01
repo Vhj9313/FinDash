0ed892b7b5445607cb50316e93d98af5f22a538a
Vhj9313 <jadhavaibhav9313@gmail.com>
2025-07-02 01:25:59 +0530

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('vite').UserConfig} */
 export default {
+  base: './', // <-- IMPORTANT for Vercel deployments
   plugins: [svelte()],
   server: {
-    // hmr: true,  // No need to manually set this
+    // hmr: true,  // not needed unless customizing HMR
   },
   build: {
     outDir: 'dist'
